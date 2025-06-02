e7fc5fdaa59c2ee0ba6a6640c7ba064aa10e0844
Guru Sai Prasad Mallepalli Gajula <glavnihonua@gmail.com>
2025-06-02 00:33:56 -0400

    checks market holidays and weekends, doesn't run on those days :)

--- Main.py
+++ Main.py
@@ -7,21 +7,69 @@ import time
 import Report
 import dotenv
 import os
+import requests
+from bs4 import BeautifulSoup
 
 PATH = r'/Users/gurusai/data/kite'
 def sleep_till9(hours,mins,seconds):
     
     return 9*3600+15*60- ( int(hours)*3600 + int(mins)*60+int(seconds) )
 
+def get_holidays():
+    """
+    Fetches the holiday dates from the Nifty Indices website.
+    
+    Returns:
+        list: A list of holiday dates as strings.
+    """
+    # URL for Nifty Indices Holiday Calendar
+    url = "https://www.niftyindices.com/resources/holiday-calendar"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+    }
+    response = requests.get(url, headers=headers, timeout=10)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    holiday_table = soup.find_all('tr')
+    dates = []
+
+
+    # Iterate through each row (skipping the header row)
+    for row in holiday_table[1:]:  # Starting from the second row
+        cols = row.find_all('td')
+        
+        # Check if there are columns in this row
+        if len(cols) > 3:
+            date = cols[1].get_text(strip=True)
+            dates.append(date)
+    return dates
+
+def is_market_open():
+    """
+    Checks if the market is open today based on the holiday calendar.
+    
+    Returns:
+        bool: True if the market is open, False if it is a holiday.
+    """
+
+    today = dt.datetime.now().strftime('%d-%b-%Y')
+    holidays = get_holidays()
+    today = dt.datetime.today()
+    
+    # Check if today is in the list of holidays
+    return today not in holidays or today.weekday() < 5  # Market is closed on weekends (Saturday=5, Sunday=6)
+
 def begin(r):
+    if not is_market_open():
+        print("Market is closed today. Exiting...")
+        r.set('end','true')
+        return
+
     print("Active threads:")
     for thread in threading.enumerate():
         print(f"Name: {thread.name}, \n\tAlive: {thread.is_alive()}\tDaemon: {thread.daemon} ")
     r.set('end','false')
-    r.set('time',dt.datetime.now(dt.timezone(dt.timedelta(hours=5,minutes= 30))).timestamp())
-    #consumer = Consumers.Consumer(directory=PATH,num_consumers=10)
+    r.set('time',dt.datetime.now(dt.timezone(dt.timedelta(hours=5,minutes= 30))).timestamp())# keep track of last tick time for watchdog
 
-   
     consumerThreads = Consumers.start_consumer_threads(PATH, num_consumers=10)
     producer_thread = threading.Thread(target=producer.heartbeat_monitor)
     
