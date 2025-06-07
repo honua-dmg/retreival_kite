754d3f25b2aa114a288dd2705bda25d3274e5b7d
Guru Sai Prasad Mallepalli Gajula <glavnihonua@gmail.com>
2025-06-07 03:07:44 -0400

    add upload and delete old-files from local storage methods to upload

--- src/upload.py
+++ src/upload.py
@@ -1,23 +1,58 @@
 import os
 import boto3
 from dotenv import load_dotenv
+import datetime as dt
 
-load_dotenv()
+class Upload():
+    def __init__(self):
+        load_dotenv()
+        # Load from .env
+        self.key = os.getenv("DIGITALOCEAN_KEY_ID")
+        self.secret = os.getenv("DIGITALOCEAN_KEY_SECRET")
+        self.region = os.getenv("DIGITALOCEAN_REGION")
+        self.endpoint = os.getenv("DIGITALOCEAN_ENDPOINT")
+        self.bucket = os.getenv("DIGITALOCEAN_BUCKET_NAME")
+        self.local_dir = "/app/data"  # Your mounted folder
 
-# Load from .env
-key = os.getenv("DO_SPACES_KEY")
-secret = os.getenv("DO_SPACES_SECRET")
-region = os.getenv("DO_SPACES_REGION")
-endpoint = os.getenv("DO_SPACES_ENDPOINT")
-bucket = os.getenv("DO_SPACES_BUCKET")
-local_dir = "data"  # Your mounted folder
-
-# Initialize S3 client
-session = boto3.session.Session()
-client = session.client(
-    's3',
-    region_name=region,
-    endpoint_url=endpoint,
-    aws_access_key_id=key,
-    aws_secret_access_key=secret
-)
+        # Initialize S3 client
+        session = boto3.session.Session()
+        self.client = session.client(
+            's3',
+            region_name=self.region,
+            endpoint_url=self.endpoint,
+            aws_access_key_id=self.key,
+            aws_secret_access_key=self.secret
+        )
+        self.bucket = 'kite'
+    def upload(self):
+        """
+        Uploads the latest CSV files to S3. (digitalocean spaces)
+        
+        This method uploads the latest CSV files to S3. It checks for files in the local directory
+        that match the current date and uploads them to S3.
+        """
+        date=dt.datetime.strftime(dt.datetime.now(dt.timezone.utc) + dt.timedelta(hours=5.5),"%Y-%m-%d")
+        for root, dirs, files in os.walk(self.local_dir):
+            for filename in files:
+                if filename.split('.')[1] != 'csv':
+                    continue
+                if filename.split('.')[0] != date:
+                    continue
+                filepath = os.path.join(root, filename)
+                s3_path = os.path.relpath(filepath, self.local_dir).replace("\\", "/")
+                print(f"Uploading {filepath} → s3://{self.bucket}/{s3_path}")
+                self.client.upload_file(filepath, self.bucket, s3_path)
+        
+    def delete_old(self):
+        """
+        Deletes old CSV files from the local directory.
+        
+        This method deletes old CSV files from the local directory that are older than 7 days.
+        """
+        cutoff_date = dt.datetime.now(dt.timezone.utc) + dt.timedelta(hours=5.5)-dt.timedelta(days=7)
+        for root, dirs, files in os.walk(self.local_dir):
+            for filename in files:
+                if filename.split('.')[1] != 'csv':
+                    continue
+                if dt.datetime.strptime(filename.split('.')[0], "%Y-%m-%d") < cutoff_date:
+                    os.remove(os.path.join(root, filename))
