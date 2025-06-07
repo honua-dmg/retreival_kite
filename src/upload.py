cdc1b9da700e56639b80d5e8ae692041b0f59f5d
Guru Sai Prasad Mallepalli Gajula <glavnihonua@gmail.com>
2025-06-07 03:31:51 -0400

    update .env.example with digital_ocean keys set up upload.py to properly handle local and cloud environments

--- src/upload.py
+++ src/upload.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 import datetime as dt
 
 class Upload():
-    def __init__(self):
+    def __init__(self,local_dir):
         load_dotenv()
         # Load from .env
         self.key = os.getenv("DIGITALOCEAN_KEY_ID")
@@ -12,7 +12,7 @@ class Upload():
         self.region = os.getenv("DIGITALOCEAN_REGION")
         self.endpoint = os.getenv("DIGITALOCEAN_ENDPOINT")
         self.bucket = os.getenv("DIGITALOCEAN_BUCKET_NAME")
-        self.local_dir = "/app/data"  # Your mounted folder
+        self.local_dir = local_dir  # Your mounted folder
 
         # Initialize S3 client
         session = boto3.session.Session()
@@ -31,28 +31,47 @@ class Upload():
         This method uploads the latest CSV files to S3. It checks for files in the local directory
         that match the current date and uploads them to S3.
         """
+        print("Uploading files to S3...")
         date=dt.datetime.strftime(dt.datetime.now(dt.timezone.utc) + dt.timedelta(hours=5.5),"%Y-%m-%d")
         for root, dirs, files in os.walk(self.local_dir):
             for filename in files:
                 if filename.split('.')[1] != 'csv':
                     continue
                 if filename.split('.')[0] != date:
+                    print(f"Skipping {filename} as it is not from today.")
                     continue
                 filepath = os.path.join(root, filename)
                 s3_path = os.path.relpath(filepath, self.local_dir).replace("\\", "/")
                 print(f"Uploading {filepath} → s3://{self.bucket}/{s3_path}")
                 self.client.upload_file(filepath, self.bucket, s3_path)
-        
+        print("Files uploaded successfully.")
     def delete_old(self):
         """
         Deletes old CSV files from the local directory.
         
         This method deletes old CSV files from the local directory that are older than 7 days.
         """
-        cutoff_date = dt.datetime.now(dt.timezone.utc) + dt.timedelta(hours=5.5)-dt.timedelta(days=7)
+        print("Deleting old files...")
+        #cutoff_date = dt.datetime.now(dt.timezone.utc) + dt.timedelta(hours=5.5)-dt.timedelta(days=7)
+        current_date = dt.datetime.now(dt.timezone(dt.timedelta(hours=5,minutes=30)))
+        cutoff_date = current_date - dt.timedelta(days=7)
         for root, dirs, files in os.walk(self.local_dir):
             for filename in files:
                 if filename.split('.')[1] != 'csv':
                     continue
-                if dt.datetime.strptime(filename.split('.')[0], "%Y-%m-%d") < cutoff_date:
+                if dt.datetime.strptime(filename.split('.')[0], "%Y-%m-%d").replace(tzinfo=dt.timezone(dt.timedelta(hours=5,minutes=30))) < cutoff_date:
                     os.remove(os.path.join(root, filename))
+        print("Old files deleted successfully.")
+
+
+def cloud_handler():
+    
+    upload = Upload("/app/data")
+    upload.upload()
+    upload.delete_old()
+    
+def local_handler(path):
+    upload = Upload(path)
+    upload.upload()
+    
+    
